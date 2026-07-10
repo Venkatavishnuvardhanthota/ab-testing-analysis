03aafba4a93921b8ad709e72b0aa0776484d116a
Thota Venkata Vishnu Vardhan <vishnuvardhanthota60@gmail.com>
2026-07-10 21:35:17 +0530

    Add: Query 4 - summary table for Z-test input

---
+++ sql/05_summary_for_ztest.sql
@@ -0,0 +1,7 @@
+SELECT
+    test_group,
+    COUNT(*) AS total_users,
+    SUM(converted) AS total_conversions,
+    ROUND(SUM(converted) / COUNT(*) * 100, 2) AS conversion_rate_pct
+FROM ab_test_raw
+GROUP BY test_group;
